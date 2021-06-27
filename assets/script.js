187ed542b88a5d16b05c888d001364a99e8b1caa
Stefan Dzhestanov <stefan.dzhestanov@clearview-intelligence.com>
2021-06-27 12:39:57 +0100

    Fixes google reCapcha

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,7 @@ timeElm.textContent = year;
 timeElm.setAttribute('datetime', year);
 
 const contact = document.querySelector('#contact-addr');
-const str =
-  'g!#sx!#sP!#s0!#sS!#sz!#sX!#sg!#sE!#s7!#sU!#sO!#sc!#se!#sG!#sV!#s1!#si!#sp!#s2!#sV!#s_!#sr!#se!#ss!#su';
+const str = 'g!#sx!#sP!#s0!#sS!#sz!#sX!#sg!#sE!#s7!#sU!#sO!#sc!#se!#sG!#sV!#s1!#si!#sp!#s2!#sV!#s_!#sr!#se!#ss!#su';
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
@@ -23,22 +22,25 @@ cardElements.forEach((elm) => {
   observer.observe(elm, { root: null, rootMargin: '0px', threshold: '50%' });
 });
 
-
-const onSubmit = token => {
-  console.log(token);
-  document.getElementById("demo-form").submit();
-}
-
 const contactForm = document.getElementById('contact-form');
-contactForm.addEventListener('submit', event => {
+contactForm.addEventListener('submit', (event) => {
+  contactForm.disabled = true;
   event.preventDefault();
-
+  contactForm.querySelector('button[type="submit"]');
   const values = {
     name: document.getElementById('name').value,
     subject: document.getElementById('subject').value,
     email: document.getElementById('email').value,
     message: document.getElementById('message').value,
-  }
-  console.log('sending email with', values)
-  emailjs.send('default_service', 'template_tyu9wv7', values, str.split('!#s').reverse().join(''));
-})
+  };
+
+  grecaptcha.ready(function () {
+    grecaptcha.execute('6LeLS10bAAAAAEE5oAINI-cSo9STuJrvDSLQY-o2', { action: 'submit' }).then((token) => {
+      if (!token) return;
+
+      contactForm.innerHTML = '<p>Thank you! We are doing our best to reply as soon as possible to your message.</p>';
+
+      emailjs.send('default_service', 'template_tyu9wv7', values, str.split('!#s').reverse().join(''));
+    });
+  });
+});
